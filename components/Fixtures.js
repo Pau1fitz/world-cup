b4d82eaabea72e8fbaeaccdf146e8292878715ed
Paul Fitzgerald <paulfitz99@gmail.com>
2018-06-12 19:54:57 +0100

    update images

--- components/Fixtures.js
+++ components/Fixtures.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, Image, View, ScrollView } from 'react-native';
 import styled from 'styled-components';
 import moment from 'moment';
 
 export default class Fixtures extends Component {
   
   state = {
-    fixtures: []
+    fixtures: [],
+    teams: []
   }
 
   componentDidMount() {
@@ -28,23 +29,27 @@ export default class Fixtures extends Component {
       <View>
         <StyledHeader />
         <ScrollView>
-          {fixtures.map((fixture, i) => {
-            const fixtureText = fixture.fixture.trim().split(' v ')
-            console.log(fixtureText)
+          {fixtures.map((f, i) => {
+            const fixtureText = f.fixture.trim().split(' v ')
+            const logo = `../images/${fixtureText[0].toLowerCase().replace(/ /g,'')}.png`
             return (
-              <View key={fixture.fixture}>
+              <View key={f.fixture}>
                 {i == 0 && (
-                  <Date>{moment(fixture.kickOffTime).format('ddd Do MMM')}</Date>
+                  <Date>{moment(f.kickOffTime).format('ddd Do MMM')}</Date>
                 )}
                 {i != 0 && moment(fixtures[i].kickOffTime).format('ddd Do MMM') != moment(fixtures[i - 1].kickOffTime).format('ddd Do MMM')  && (
-                  <Date>{moment(fixture.kickOffTime).format('ddd Do MMM')}</Date>
+                  <Date>{moment(f.kickOffTime).format('ddd Do MMM')}</Date>
                 )}
                 <FixtureTextContainer>
                   <FlexView>
+                  <Image
+                    style={{width: 20, height: 20}}
+                    source={require('../images/argentina.png')}
+                  />
                     <TeamText>{fixtureText[0]}</TeamText>
                   </FlexView>
                   <FlexView>
-                    <FixtureText>{moment(fixture.kickOffTime).format('HH:mm')}</FixtureText>
+                    <FixtureText>{moment(f.kickOffTime).format('HH:mm')}</FixtureText>
                   </FlexView>
                   <FlexView>
                     <TeamText>{fixtureText[1]}</TeamText>
@@ -76,6 +81,11 @@ const Date = styled.Text`
   margin-bottom: 2px;
 `
 
+const Flag = styled.Image`
+  width: 20px;
+  height: 20px;
+`
+
 const FixtureTextContainer = styled.View`
   display: flex;
   flex-direction: row;
@@ -90,7 +100,7 @@ const FlexView = styled.View`
 const FixtureText = styled.Text`
   color: rgb(51, 51, 51);
   padding: 10px;
-  flex-basis: 20%;
+  flex-basis: 29%;
 `
 
 const TeamText = styled.Text`
