bd438271ab826f154a4066ce312b763bfb3200cc
Paul Fitzgerald <paulfitz99@gmail.com>
2018-06-14 23:22:47 +0100

    eject

--- components/ListHOC.js
+++ components/ListHOC.js
@@ -32,19 +32,20 @@ export default HOCList = (title, api) => {
       }
       
       return (
-        <View>
+        <ItemContainer>
           {items.map((item, i) => {
             const flag = `https://raw.githubusercontent.com/Pau1fitz/world-cup/master/images/${item.team.toLowerCase().replace(/ /g,'')}.png`
             return (
-              <ItemContainer key={i}>
+              <ItemBox key={i}>
+                <ItemPosition>{i + 1}</ItemPosition>
                 <Flag source={{uri: flag}}/>
                 <ItemText>{item.name}</ItemText>
                 <ItemText>{item.team}</ItemText>
                 <ItemText>{item.amount}</ItemText>
-              </ItemContainer>
+              </ItemBox>
             )
           })}
-        </View>
+        </ItemContainer>
       )
     }
   }
@@ -59,17 +60,30 @@ const ItemContainer = styled.View`
   shadow-opacity: 0.8;
   shadow-radius: 2;
   margin-bottom: 10px;
+`
+const ItemBox = styled.View`
+  align-items: center;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
+  border-bottom-color: #eee;
+  border-bottom-width: 1px;
 `
 const Flag = styled.Image`
   width: 26px;
   height: 26px;
+  margin-left: 10px;
+`
+const ItemPosition = styled.Text`
+  color: #000;
+  padding: 10px;
+  font-weight: 800;
+  font-size: 14px;
+  width: 20px;
 `
 const ItemText = styled.Text`
   color: #000;
   padding: 10px;
   font-weight: 800;
   font-size: 14px;
+  flex-basis: 38%;
 `
